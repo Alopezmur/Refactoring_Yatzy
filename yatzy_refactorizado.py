444f1254976e833b82f2f1134854900f0f2c5364
Alejnadro Lopez <alopezmur@cifpfbmoll.eu>
2021-01-05 18:27:23 +0100

    refactor: refactorizar def twos

--- yatzy_refactorizado.py
+++ yatzy_refactorizado.py
@@ -24,16 +24,9 @@ class Yatzy:
     @staticmethod
     def twos(*throw):
         sum = 0
-        if (d1 == 2):
-            sum += 2
-        if (d2 == 2):
-            sum += 2
-        if (d3 == 2):
-            sum += 2
-        if (d4 == 2):
-            sum += 2
-        if (d5 == 2):
-            sum += 2
+        for dice in throw:
+            if dice == 2:
+                sum += 2
         return sum
 
     @staticmethod
